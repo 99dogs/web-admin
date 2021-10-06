408519b1e13a71646bd484cd0a370ca509f69f26
Luis Covelo <luis.covelo@hotmail.com>
2021-10-06 09:45:23 -0300

    ajustes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
+const nomeApp = process.env.npm_package_name;
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/admin-99dogs'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/admin-99dogs/index.html'));
+app.get('/*', function(req,res) {    
+	res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 // Start the app by listening on the default Heroku port
